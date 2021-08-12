ff0fa00327a986cd9f99202160cd9b33d87a5e41
Monique Rodrigues <moniqueoliveira0104@gmail.com>
2021-08-12 15:51:45 -0300

    adicionado botão

--- Documents/CURSOS TI/everis/angularJs/desafio1/controller.js
+++ Documents/CURSOS TI/everis/angularJs/desafio1/controller.js
@@ -2,6 +2,11 @@ angular.module("helloWorld", []); //criando o modulo
 
 // localiza o modulo
 angular.module("helloWorld").controller("helloWorldCtrl", function($scope){
-    $scope.message = "Hello World !!!"
+
+
+    $scope.mostrarAlert= function(){
+        $scope.message = "Hello World !!!"
+    }
+
 
 }); 
