0138afad92abeef8ad91af11b2f5634b6b66ca1e
Monique Rodrigues <moniqueoliveira0104@gmail.com>
2021-08-18 12:45:24 -0300

    criacao lista de produtos

--- Documents/CURSOS TI/everis/angularJs/desafio2/controller.js
+++ Documents/CURSOS TI/everis/angularJs/desafio2/controller.js
@@ -6,13 +6,13 @@ $scope.titulo="Lojinha Virtual do Só Vamu";
 $scope.busca= "O que você está procurando?";
 
 
+$scope.listaProdutos = [
+    {produto: "sabão em pó",descricao: "bababababa", valor:"R$150,00"},
+    {produto: "amaciante",descricao: "bebebebebe", valor:"R$160,00"},
+    {produto: "detergente",descricao: "bibibibibi", valor:"R$170,00"},
+    {produto: "Vassoura",descricao: "bibibibibi", valor:"R$170,00"},
 
-
-
-
-
-
-
+];
 
 })
 
