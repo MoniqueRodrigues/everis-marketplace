e59baff66f61413d3c547cd955bf7453e251f7e6
Monique Rodrigues <moniqueoliveira0104@gmail.com>
2021-08-23 13:52:41 -0300

    adicionado itens em opções

--- Documents/CURSOS TI/everis/angularJs/desafio2/controller.js
+++ Documents/CURSOS TI/everis/angularJs/desafio2/controller.js
@@ -3,9 +3,21 @@ angular.module("lojaModulo").controller("indexController",function($scope){
     
 
 $scope.titulo="Lojinha Virtual do Só Vamu";
-$scope.busca= "O que você está procurando?";
 
 
+//filtro(opções)
+$scope.tituloOpcoes= "O que você está procurando?"
+$scope.ordenacao= function(){
+
+   
+
+
+    }
+
+
+
+
+//busca e filtragem
 $scope.listaProdutos = [
     {produto: "sabão em pó",descricao: "bababababa", valor:"R$150,00"},
     {produto: "amaciante",descricao: "bebebebebe", valor:"R$160,00"},
