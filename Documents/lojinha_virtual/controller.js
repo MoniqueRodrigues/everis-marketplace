e6fef4dda550381ceb7d28a63ec2f2bf2457a397
Monique Rodrigues <moniqueoliveira0104@gmail.com>
2021-08-25 16:26:49 -0300

    botão switch habilitado

--- Documents/lojinha_virtual/controller.js
+++ Documents/lojinha_virtual/controller.js
@@ -1,31 +1,49 @@
-angular.module("lojaModulo",[]);
-angular.module("lojaModulo").controller("indexController",function($scope){
-    
+angular.module("lojaModulo", []);
+angular.module("lojaModulo").controller("indexController", function ($scope) {
 
-$scope.titulo="Lojinha Virtual do Só Vamu";
 
-// //switch
-// $scope.editar= false;
+    $scope.titulo = "Lojinha Virtual do Só Vamu";
 
 
 
+    //switch
+    // define a visibilidade inicial
+    $scope.visivel = false;
+    //função para exibir o elemento
+    $scope.exibir = function () {
+        $scope.visivel = !$scope.visivel;
+
+    }
 
 
-//filtro(opções)
-$scope.tituloBusca= "O que você está procurando?"
+
+
+
+    //filtro(opções)
+    $scope.tituloBusca = "O que você está procurando?"
 
 
 
 
 
-//busca e filtragem
-$scope.listaProdutos = [
-    {produto: "sabão em pó",descricao: "bababababa", valor:"R$150,00"},
-    {produto: "amaciante",descricao: "bebebebebe", valor:"R$160,00"},
-    {produto: "detergente",descricao: "bibibibibi", valor:"R$170,00"},
-    // {produto: "Vassoura",descricao: "bibibibibi", valor:"R$170,00"},
+    //busca e filtragem
+    $scope.listaProdutos = [{
+            produto: "sabão em pó",
+            descricao: "bababababa",
+            valor: "R$150,00"
+        },
+        {
+            produto: "amaciante",
+            descricao: "bebebebebe",
+            valor: "R$160,00"
+        },
+        {
+            produto: "detergente",
+            descricao: "bibibibibi",
+            valor: "R$170,00"
+        },
+        // {produto: "Vassoura",descricao: "bibibibibi", valor:"R$170,00"},
 
-];
+    ];
 
 })
-
